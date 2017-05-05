9060c545c1258b26c580058e89c523720fd40b36
Helen <kershaw@brown.edu>
2017-05-05 10:38:14 -0400

    Molpro test. Submits molpro job. Dependent job waits for finish, greps for run time and errors

---
+++ master_molpro.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo "Starting master"
+
+rm -r molpro
+mkdir molpro
+cp tlbr_mp2_dft_so.test molpro
+
+cat << EOF2 > check_results.sh
+#!/bin/bash
+#SBATCH -n 1
+#SBATCH -o ../molpro.result.out
+grep real molpro-test.out
+
+if ! grep ERRORS test.log; then
+echo "FAILED: errors in molpro run"
+fi 
+
+EOF2
+
+
+cat <<\EOF > molpro.sh
+#!/bin/bash
+
+#SBATCH -N 2
+#SBATCH --ntasks-per-node=4
+#SBATCH -o molpro-test.out
+#SBATCH -e molpro-test.out
+#SBATCH -D molpro
+
+module load Molpro/2015_gcc
+module load mpi/mvapich2-2.0rc1_gcc
+
+sbatch --dependency=afterok:$SLURM_JOB_ID ../check_results.sh
+
+time molpro -n 8 tlbr_mp2_dft_so.test
+EOF
+
+sbatch molpro.sh
+myq
