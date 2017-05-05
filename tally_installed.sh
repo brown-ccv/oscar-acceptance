6c14bec415b539cccfae7b75441c714e92582b4f
Helen <kershaw@brown.edu>
2017-05-05 10:37:33 -0400

    Count up installed modules and compare to centos6

--- tally_installed.sh
+++ tally_installed.sh
@@ -4,4 +4,8 @@
 
 ls /gpfs/runtime/modulefiles > rh7_mods
 
-grep -Fxv -f rh7_mods centos6_mods
+grep -Fxv -f rh7_mods centos6_mods 
+
+echo " "
+echo -n "total: "
+grep -Fxvc -f rh7_mods centos6_mods 
