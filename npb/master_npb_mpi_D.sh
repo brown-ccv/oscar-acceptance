396161bb16fce86e805ee350b9297bd3219eac31
Helen <kershaw@brown.edu>
2017-05-19 17:04:11 -0400

    MV2_USE_BLOCKING=1  seems to stop the hang

--- npb/master_npb_mpi_D.sh
+++ npb/master_npb_mpi_D.sh
@@ -5,6 +5,8 @@
 TESTDIR=$PWD
 NPBDIR=/users/hkershaw/scratch/centos7/acceptance_tests/npb/NPB3.3.1/NPB3.3-MPI
 MPIMOD=mpi/mvapich2-2.0rc1_intel
+#MPIMOD=mpi/mvapich2-2.0rc1_gcc
+#MPIMOD=mpi/openmpi_2.0.1_intel  
 
 cd $NPBDIR
 module load $MPIMOD
@@ -19,14 +21,17 @@ cat <<EOF > npb.sh
 
 #SBATCH -N 4-4 
 #SBATCH --ntasks-per-node=16
-#SBATCH -o $TESTDIR/npb-test.out
-#SBATCH -e $TESTDIR/npb-test.out
+#SBATCH -o $TESTDIR/npb-test-D.%j.out
+#SBATCH -e $TESTDIR/npb-test-D.%j.out
 #SBATCH -D $NPBDIR
 #SBATCH -t 1:00:00
 
 module load $MPIMOD
 
-time srun -n 64 bin/bt.D.64
+#export MV2_USE_SHMEM_COLL=0
+export MV2_USE_BLOCKING=1
+
+srun -n 64 bin/bt.D.64
 
 EOF
 
