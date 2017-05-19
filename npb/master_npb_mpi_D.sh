a36eb0c2f318ee7e367b39ba79e030e4c8a42dbd
Helen <kershaw@brown.edu>
2017-05-19 11:27:52 -0400

    NPB benchmarks C and D fh7 and D centos6

---
+++ npb/master_npb_mpi_D.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -x
+
+#AIM: compile and run npb mpi benchmarks and record the time
+
+TESTDIR=$PWD
+NPBDIR=/users/hkershaw/scratch/centos7/acceptance_tests/npb/NPB3.3.1/NPB3.3-MPI
+MPIMOD=mpi/mvapich2-2.0rc1_intel
+
+cd $NPBDIR
+module load $MPIMOD
+
+make clean
+make bt NPROCS=64 CLASS=D
+
+cd $TESTDIR 
+
+cat <<EOF > npb.sh
+#!/bin/bash
+
+#SBATCH -N 4-4 
+#SBATCH --ntasks-per-node=16
+#SBATCH -o $TESTDIR/npb-test.out
+#SBATCH -e $TESTDIR/npb-test.out
+#SBATCH -D $NPBDIR
+#SBATCH -t 1:00:00
+
+module load $MPIMOD
+
+time srun -n 64 bin/bt.D.64
+
+EOF
+
+sbatch npb.sh
+myq
+
