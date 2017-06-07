5e28ac6557b7b3af61798b8f693a01237de1d9ed
Helen <kershaw@brown.edu>
2017-06-07 15:58:24 -0400

    check nodes for yum list installed consitency

--- check_yum.sh
+++ check_yum.sh
@@ -8,14 +8,18 @@
 
 #global:
 count=0
+rm -r yumtests
+mkdir yumtests
+cd yumtests
 testdir=$(pwd)
+
 
 # function to check if counts are different
 yumdiff () {
 
    lines=`wc -l < $1`
    if ((count == 0)); then  #first time 
-     echo $lines
+     echo $lines $1
      count=$lines
    else # already got a line count
       if((count != $lines)); then
@@ -33,7 +37,6 @@ for n in "${node[@]}"
 do
 
    ssh -T $n << EOF
-   echo $HOSTNAME
    cd $testdir
    module unload python
    yum list installed > $n.out
@@ -52,7 +55,20 @@ loginnodes=("login003" "login004")
 # compute nodes
 computenodes=("node401" "node402" "node403" "node404" "node405" "node406" "node407" "node409" \
               "node410" "node411" "node412" "node413" "node414" "node415" "node416" "node417" \
-              "node418" "node419" "node420" "node421" "node422" "node423" "node424")
+              "node418" "node419" "node420" "node421" "node422" "node423" "node424" "smp015")
 
+# gpunodes
+gpunodes=("gpu001" "gpu002")
+
+# vncnodes
+vncnodes=("node408" "cave020")
+
+count=0
 nodeloop "${loginnodes[@]}"
+count=0
+nodeloop "${computenodes[@]}"
+count=0
+nodeloop "${gpunodes[@]}"
+count=0
+nodeloop "${vncnodes[@]}"
 
