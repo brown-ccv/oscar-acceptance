fdc1d643e4a9c2c00732f8acf7ffb4a08768ad79
Helen <kershaw@brown.edu>
2017-08-24 09:24:31 -0400

    mpi tests. separate for 1.8.3 openmpi as this does not use srun

--- run_mpi.sh
+++ run_mpi.sh
@@ -29,17 +29,30 @@ date > $resultsfile
 # Slurm commands - pgi has a bug that affects openmpi
 #mods=("mpi/mvapich2-2.3a_gcc" "mpi/mvapich2-2.3a_intel" "mpi/mvapich2-2.3a_pgi")
 #mods=("mpi/openmpi_2.0.3_gcc"   "mpi/openmpi_2.0.3_intel" "mpi/openmpi_2.0.3_pgi")
-mods=("mpi/openmpi_1.8.3_gcc") 
-#mods=("mpi/mvapich2-2.3a_intel" "mpi/mvapich2-2.3a_intel" "mpi/mvapich2-2.3a_pgi" \
-#      "mpi/openmpi_2.0.3_gcc"   "mpi/openmpi_2.0.3_intel" "mpi/openmpi_2.0.3_pgi" \
-#      "openmpi_1.8.3_gcc")
+mods=("mpi/mvapich2-2.3a_intel" "mpi/mvapich2-2.3a_gcc" "mpi/mvapich2-2.3a_pgi" \
+      "mpi/openmpi_2.0.3_gcc"   "mpi/openmpi_2.0.3_intel" )
+
+# mpi/openmpi_2.0.3_pgi
 
 for i in "${mods[@]}"
 do
    module load $i
-   rm a.out
+   rm a.out test.err
    mpicc hello_c.c
    salloc -N 2 srun --mpi=pmi2 ./a.out > test.err 
    mpi_errors test.err $i
    module unload $i
 done
+
+#mods=("mpi/openmpi_1.8.3_gcc") 
+#for i in "${mods[@]}"
+#do
+#   module load $i
+#   rm a.out
+#   mpicc hello_c.c
+#   salloc -N 2 mpirun ./a.out > test.err 
+#   mpi_errors test.err $i
+#   module unload $i
+#done
+#
+
