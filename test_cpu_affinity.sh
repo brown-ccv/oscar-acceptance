6ab6bb0a3b0767e34e4fb7a6811537fff6908eca
Helen <kershaw@brown.edu>
2017-09-15 14:43:49 -0400

    test for cpu affinity.  Not testing correctly for two nodes at the moment.

---
+++ test_cpu_affinity.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Aim: test the processor affinity 
+s=$(basename -- "$0")
+resultsfile="results."${s%.*}
+temporaryfile=affinity.out
+
+affinity_check() {
+  while read result
+  do
+    proc=${result#*.}
+    cpu=${result%.*}
+    #echo "proc" $proc
+    #echo "cpu" $cpu 
+    if [ $proc -ne $cpu ]; then
+       echo FAILED: cpu affinity binding > $resultsfile
+       break
+    fi
+  done < $1
+}
+
+date > $resultsfile
+
+# compile affinity code
+module load mpi/mvapich2-2.3a_gcc
+cd affinity
+make
+cd ..
+
+salloc -N 2 --ntasks-per-node=4 srun --mpi=pmi2 affinity/get_cpu > $temporaryfile
+
+echo $MV_ENABLE_AFFINITY
+affinity_check $temporaryfile
