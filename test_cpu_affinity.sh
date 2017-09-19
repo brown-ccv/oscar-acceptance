72a16b56527d147173668054830eb535ef56684d
Helen <kershaw@brown.edu>
2017-09-19 11:18:26 -0400

    Test for cpu affinity

--- test_cpu_affinity.sh
+++ test_cpu_affinity.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Aim: test the processor affinity 
+#  This test uses 2 Nodes with 4 tasks each in exclusive mode
+
 s=$(basename -- "$0")
 resultsfile="results."${s%.*}
 temporaryfile=affinity.out
@@ -8,14 +10,23 @@ temporaryfile=affinity.out
 affinity_check() {
   while read result
   do
-    proc=${result#*.}
-    cpu=${result%.*}
-    echo "proc" $proc
-    echo "cpu" $cpu 
-    if [ $proc -ne $cpu ]; then
-       echo FAILED: cpu affinity binding > $resultsfile
-       break
+    cpu=${result#*.}
+    task=${result%.*}
+    # Node 1 
+    if [ $task -lt 4 ]; then
+      if [ $task -ne $cpu ]; then
+         echo FAILED: cpu affinity binding node 1 $task:$cpu >> $resultsfile
+         #break
+      fi
     fi
+    # Node 2
+    if [ $task -ge 4 ]; then
+      if [ $task -ne $((cpu + 4)) ]; then
+         echo FAILED: cpu affinity binding node 2 $task:$cpu >> $resultsfile
+         #break
+      fi  
+    fi  
+
   done < $1
 }
 
@@ -23,11 +34,12 @@ date > $resultsfile
 
 # compile affinity code
 module load mpi/mvapich2-2.3a_gcc
+export MV2_ENABLE_AFFINITY=0
 cd affinity
 make
 cd ..
 
 salloc -N 2 --exclusive --ntasks-per-node=4 srun --mpi=pmi2 affinity/get_cpu > $temporaryfile
 
-echo $MV_ENABLE_AFFINITY
+echo "MV2_ENABLE_AFFINITY="$MV2_ENABLE_AFFINITY | tee -a $resultsfile
 affinity_check $temporaryfile
