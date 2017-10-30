5e5478c7da1e6870c0101d3ea6d10adb175d779b
Helen <helen_kershaw@brown.edu>
2017-10-30 15:58:57 -0400

    ccvreadme test outputs to resultsfile

--- test_ccvreadme.sh
+++ test_ccvreadme.sh
@@ -11,9 +11,9 @@ for mod in $(ls /gpfs/runtime/opt/)
 do
 
    if [[  -z `find /gpfs/runtime/opt/$mod -maxdepth 2 -name CCVREADME` ]] && [[  -z `find /gpfs/runtime/opt/$mod -maxdepth 2 -name CCV_README` ]]; then
-     echo -n "FAILED: " $mod 
-     echo -n  "  installed by : "
-     ls -ld /gpfs/runtime/opt/$mod | awk '{print $3}'
+     echo -n "FAILED: " $mod >> $resultsfile
+     echo -n  "  installed by : " >> $resultsfile
+     ls -ld /gpfs/runtime/opt/$mod | awk '{print $3}' >> $resultsfile
    fi
    
 done
