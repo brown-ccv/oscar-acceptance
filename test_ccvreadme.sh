2ced7c3dc983e2648385e8811b1cdab564553193
Helen <helen_kershaw@brown.edu>
2017-10-30 15:55:08 -0400

    test for CCVREADME (or CCV_README) in module directory. Fail lists owner of directory

---
+++ test_ccvreadme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Aim: check if module directories have a CCVREADME or a CCV_README
+
+s=$(basename -- "$0")
+resultsfile="results."${s%.*}
+
+date > $resultsfile
+
+for mod in $(ls /gpfs/runtime/opt/)
+do
+
+   if [[  -z `find /gpfs/runtime/opt/$mod -maxdepth 2 -name CCVREADME` ]] && [[  -z `find /gpfs/runtime/opt/$mod -maxdepth 2 -name CCV_README` ]]; then
+     echo -n "FAILED: " $mod 
+     echo -n  "  installed by : "
+     ls -ld /gpfs/runtime/opt/$mod | awk '{print $3}'
+   fi
+   
+done
